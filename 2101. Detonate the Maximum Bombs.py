30a6e277cda703b0eb2b03e54348c9ce00122349
Maheshdh <36495289+Maheshdh@users.noreply.github.com>
2024-09-14 01:53:58 -0400

    Update 2101. Detonate the Maximum Bombs.py

--- 2101. Detonate the Maximum Bombs.py
+++ 2101. Detonate the Maximum Bombs.py
@@ -2,10 +2,10 @@ class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
         can_detonate = {}
         for i in range(len(bombs)):
-            for j in range(i + 1, len(bombs)):
-                x1, y1, r1 = bombs[i]
-                x2, y2, r2 = bombs[j]
-                dist = sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+            for j in range(i+1, len(bombs)):
+                x1,y1,r1 = bombs[i]
+                x2,y2,r2 = bombs[j]
+                dist = sqrt((x1-x2)**2 + (y1-y2)**2)
                 if dist <= r1:
                     if i in can_detonate:
                         can_detonate[i].append(j)
@@ -16,21 +16,18 @@ class Solution:
                         can_detonate[j].append(i)
                     else:
                         can_detonate[j] = [i]
-        print(can_detonate)
         res = 0
-
-        def dfs(i, visit):
-            if i in visit:
+        def dfs(s, visit):
+            if s in visit:
                 return 0
-            visit.add(i)
-            bombs = 1
-            neighbors = []
-            if i in can_detonate:
-                neighbors = can_detonate[i]
-            for nei in neighbors:
-                bombs += dfs(nei, visit)
-            return bombs
+            visit.add(s)
+            bombs_detonated = 1
+            for neighbor in can_detonate.get(s,[]):
+                bombs_detonated += dfs(neighbor,visit)
+            return bombs_detonated
 
         for i in range(len(bombs)):
-            res = max(res, dfs(i, set()))
+            res = max(res, dfs(i,set()))
+        
         return res
+        
