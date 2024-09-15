736ab436e69c8efe568a1e74680a0f668857504d
Maheshdh <36495289+Maheshdh@users.noreply.github.com>
2024-09-14 22:12:52 -0400

    Update 1146. Snapshot Array.py

--- 1146. Snapshot Array.py
+++ 1146. Snapshot Array.py
@@ -1,31 +1,28 @@
 class SnapshotArray:
 
     def __init__(self, length: int):
-        self.records = [[(0,0)] for i in range(length)]
+        self.snap_array = [[(0,0)] for i in range(length)]
         self.snap_id = 0
 
     def set(self, index: int, val: int) -> None:
-        self.records[index].append((self.snap_id,val))
+        self.snap_array[index].append((self.snap_id,val))
 
     def snap(self) -> int:
         self.snap_id += 1
-        return self.snap_id - 1
-        
+        return self.snap_id - 1       
 
     def get(self, index: int, snap_id: int) -> int:
-        record = self.records[index]
-        low = 1
+        record = self.snap_array[index]
+        low = 0
         high = len(record) - 1
-        ans = None
         while low <= high:
             mid = (low + high)//2
             if snap_id >= record[mid][0]:
-                ans = record[mid][1]
                 low = mid + 1
             else:
                 high = mid - 1
         return record[high][1]
-
+    
 
 # Your SnapshotArray object will be instantiated and called as such:
 # obj = SnapshotArray(length)
