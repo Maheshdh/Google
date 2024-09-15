a651ae2ae63ee334492702449a1bcdfcea1ee4e9
Maheshdh <36495289+Maheshdh@users.noreply.github.com>
2024-09-15 11:54:53 -0400

    Update 2018. Check if Word Can Be Placed In Crossword.py

--- 2018. Check if Word Can Be Placed In Crossword.py
+++ 2018. Check if Word Can Be Placed In Crossword.py
@@ -1,13 +1,23 @@
 class Solution:
     def placeWordInCrossword(self, board: List[List[str]], word: str) -> bool:
-        words = [word, word[::-1]]
-        for B in board,zip(*board):
-            for row in B:
-                segs = "".join(row).split("#")
-                for w in words:
-                    for seg in segs:
-                        if len(seg) == len(w):
-                            if all(seg[i] == w[i] or seg[i] == ' ' for i in range(len(w))):
-                                return True
+        return self.canPlace(board,word) or self.canPlace(zip(*board),word)
+    
+    def canPlace(self,board,word):
+        # T : O(len(word))
+        words = [word,word[::-1]]
+        print("words ",words)
+        # T O(rows)
+        for row in board:
+            #print("row = ",row)
+            segs = "".join(row).split("#")
+            #print("Segs = ",segs)
+            for w in words:
+                # O(cols)
+                for seg in segs:
+                    if len(seg) == len(w):
+                        # O(len(word))
+                        if all(seg[i] == w[i] or seg[i] == " " for i in range(len(w))):
+                            return True
         return False
+
         
